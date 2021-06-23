3740d6336cb7db3452ce58209d206da9fd061a83
Charlie Traylor <ctraylor@tql.com>
2021-06-23 15:16:39 -0400

    angular counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter';
+  
+  counter: number = 0;
+
+
+  add(): void
+  {
+    this.counter++;
+    this.display();
+  }
+  
+  sub(): void
+  {
+    this.counter--;
+    this.display();
+  }
+
+  inpClass: string = "";
+  display(): void
+  {
+    this.inpClass = "";
+    if(this.counter % 2 == 0) this.inpClass += " text-danger "
+    if(this.counter % 3 == 0) this.inpClass += " fw-bold "
+    if(this.counter % 7 == 0) this.inpClass += " fst-italic "
+  }
+  
+
 }
